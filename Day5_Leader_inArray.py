36d66f0ad7b304bd235cc6d27e69d6fdd2d3e3fa
Abhishek-212 <abhishek212kalekar@gmail.com>
2024-09-13 21:38:25 +0530

    Update Day5_Leader_inArray.py

--- Day5_Leader_inArray.py
+++ Day5_Leader_inArray.py
@@ -2,7 +2,7 @@ def find_leader(arr, n):
     leader = []
     
     max_from_right = arr[-1]       # seting last element as leader
-    leader.append(max_from_right)
+    leader.append(max_from_right)  # for(start, stop, steps)
 
     for i in range(n-1, -1, -1):
         if arr[i] > max_from_right:
