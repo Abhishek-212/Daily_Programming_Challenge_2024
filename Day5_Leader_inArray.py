76bfa70f1e97cfb8c380d0f28cedc51fef586f0b
Abhishek-212 <abhishek212kalekar@gmail.com>
2024-09-13 21:36:49 +0530

    Add files via upload

---
+++ Day5_Leader_inArray.py
@@ -0,0 +1,17 @@
+def find_leader(arr, n):
+    leader = []
+    
+    max_from_right = arr[-1]       # seting last element as leader
+    leader.append(max_from_right)
+
+    for i in range(n-1, -1, -1):
+        if arr[i] > max_from_right:
+            leader.append(arr[i])
+            max_from_right = arr[i]
+    leader.reverse()
+    print(leader)
+
+
+arr = [1,2,3,1000000]
+n = len(arr)
+find_leader(arr, n)
